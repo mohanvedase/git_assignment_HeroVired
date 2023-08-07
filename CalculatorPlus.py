594dc5d8a2023fe495789ffd4c6d51bee4ad9088
mohankrishna <mohankandikuppa119@gmail.com>
2023-08-07 21:07:23 +0530

    code 1 for calculatorplus app

---
+++ CalculatorPlus.py
@@ -0,0 +1,24 @@
+import math
+
+class Calculator:
+    def add(self, a, b): 
+        return a + b
+    def subtract(self, a, b): 
+        return a - b
+    def multiply(self, a, b):
+        return a * b
+    def divide(self, a, b): 
+        return a/b  
+
+if "__name__" == "__main__":
+    calculator = Calculator()
+num1 = 16
+num2 = 4
+
+
+calculator = Calculator()
+
+print(f"{num1} + {num2} = {calculator.add(num1, num2)}") 
+print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
+print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
+print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
