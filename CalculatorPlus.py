44094e01b0199223b9e2514108ffabecf9a4778e
mohankrishna <mohankandikuppa119@gmail.com>
2023-08-07 21:19:18 +0530

    bug is detected and fixed in code 1

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -8,7 +8,9 @@ class Calculator:
     def multiply(self, a, b):
         return a * b
     def divide(self, a, b): 
-        return a/b  
+         if b ==0:
+            raise ValueError("Cannot divide by zero")
+         return a/b  
 
 if "__name__" == "__main__":
     calculator = Calculator()
