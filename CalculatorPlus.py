ffe069af955586a17b7ce04d52109334d341b819
mohankrishna <mohankandikuppa119@gmail.com>
2023-08-07 21:27:21 +0530

    square_root function is added to code

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -8,12 +8,17 @@ class Calculator:
     def multiply(self, a, b):
         return a * b
     def divide(self, a, b): 
-        return a/b  
+        if b ==0:
+            raise ValueError("Cannot divide by zero")
+        return a/b 
+    def square_root(self,x):
+        return math.sqrt(x)  
 
 if "__name__" == "__main__":
     calculator = Calculator()
 num1 = 16
 num2 = 4
+num3 = 25
 
 
 calculator = Calculator()
@@ -22,3 +27,4 @@ print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
 print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}") 
 print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
 print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+print(f"The square root of{num3} = {calculator.square_root(num3)}")
