49e8dcc02bb237854d8630e9abdf589dc13657f6
mohankrishna <mohankandikuppa119@gmail.com>
2023-08-08 10:54:24 +0530

    committing stashed changesin myfile

---
+++ geometry-calculator.py
@@ -0,0 +1,12 @@
+import math
+class GeometryCalculator:
+    def calculate_circle_area(self, radius):
+        return math.pi * radius ** 2
+   
+if "__name__" == "__main__": 
+    calculator = GeometryCalculator()
+ 
+radius = 5
+calculator = GeometryCalculator()
+
+print(f"The area of the circle with radius {radius} ={calculator.calculate_circle_area(radius)}")
